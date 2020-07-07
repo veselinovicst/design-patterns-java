169ad461a06cdddd2df4c940b7841ca2f1677287
Stefan Veselinovic <stefanveselinovic@Stefans-MacBook-Pro.local>
2020-07-07 16:09:30 +0200

    Added bridge pattern

---
+++ src/main/structural/bridge/BridgeDemo.java
@@ -0,0 +1,8 @@
+package main.structural.bridge;
+
+/**
+ * Decouple an abstraction from its implementation so that the two can vary independently.
+ * Publish interface in an inheritance hierarchy, and bury implementation in its own inheritance hierarchy.
+ */
+public class BridgeDemo {
+}
---
+++ src/main/structural/bridge/DrawAPI.java
@@ -0,0 +1,5 @@
+package main.structural.bridge;
+
+public interface DrawAPI {
+    public void drawCircle(int radius, int x, int y);
+}
